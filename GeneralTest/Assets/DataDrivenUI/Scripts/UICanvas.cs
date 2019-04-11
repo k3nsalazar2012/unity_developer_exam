51207681c6701ac74407cdb67036ee2bc1b76335
devken <31090803+k3nsalazar2012@users.noreply.github.com>
2019-04-11 08:41:29 +0800

    First Commit
    Loading exam files

---
+++ GeneralTest/Assets/DataDrivenUI/Scripts/UICanvas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+namespace CanvasData
+{
+    public class UICanvas : MonoBehaviour
+    {
+        #region Property Fields
+        [SerializeField]
+        private int CurrentIndex = 0;
+
+        [SerializeField]
+        private UIData CurrentUIData;
+
+        [SerializeField]
+        private List<UIData> UIDataList;
+        
+        #endregion
+
+        #region Unity Life Cycle
+        private void Awake()
+        {
+            LoadData(0);
+        }
+        #endregion
+
+        #region Private Functionality
+        private void LoadData(int index)
+        {
+            CurrentIndex = Mathf.Clamp(index, 0, UIDataList.Count - 1);
+            CurrentUIData = UIDataList[CurrentIndex];
+            GenerateUI();
+        }
+        #endregion
+
+        #region Unity Button Events
+        public void Prev()
+        {
+            LoadData(CurrentIndex - 1);
+        }
+
+        public void Next()
+        {
+            LoadData(CurrentIndex + 1);
+        }
+        #endregion
+
+        // *************************************************************************************************************
+        // Generate UI here based on the current data
+        // *************************************************************************************************************
+        private void GenerateUI()
+        {
+            
+        }
+    }
+}
---
+++ GeneralTest/Assets/DataDrivenUI/Scripts/UIData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+namespace CanvasData
+{
+    [CreateAssetMenu]
+    public class UIData : ScriptableObject
+    {
+        [Serializable]
+        public class RangeData
+        {
+            public int Min;
+            public int Max;
+            public int Val;
+        }
+
+        [Serializable]
+        public class ColorData
+        {
+            public Color Color;
+            public string Letter;
+        }
+
+        [SerializeField]
+        private List<string> _Items;
+        public List<string> Items
+        {
+            get { return _Items; }
+        }
+
+        [SerializeField]
+        private List<RangeData> _RangeList;
+        public List<RangeData> RangeList
+        {
+            get { return _RangeList; }
+        }
+
+        [SerializeField]
+        private List<ColorData> _Colors;
+        public List<ColorData> Colors
+        {
+            get { return _Colors; }
+        }
+    }
+}
